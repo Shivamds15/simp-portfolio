6e5ef3336463684c4f7abb7e8421beb89c622e60
SHIVAM SINGH <157016461+Shivamds15@users.noreply.github.com>
2024-07-04 02:27:37 +0530

    Delete portfolio directory

--- portfolio/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var typed = new Typed(".typing", {
-        strings: ["Student", "YouTuber", "Developer", "Designer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    });
