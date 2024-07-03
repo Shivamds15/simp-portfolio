ac58b367e65729f196f5119278ab227d588bc895
SHIVAM SINGH <157016461+Shivamds15@users.noreply.github.com>
2024-07-03 12:56:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Student", "YouTuber", "Developer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    });
