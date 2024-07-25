bd50fd61377189848bcbe4e603b02464b637a079
SHIVAM SINGH <157016461+Shivamds15@users.noreply.github.com>
2024-07-25 14:14:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ $(document).ready(function(){
         loop: true
     });
     });
+
+document.getElementById('year').textContent = new Date().getFullYear();const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  fetch('https://formspree.io/shivamsingh910672@gmail.com', {
+    method: 'POST',
+    body: formData,
+  })
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
+});
