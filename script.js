e4012a85ee5abbf871815c5dcde4378c3397d6ea
SHIVAM SINGH <157016461+Shivamds15@users.noreply.github.com>
2024-07-25 14:42:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ $(document).ready(function(){
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
-        document.getElementById('year').textContent = new Date().getFullYear();const form = document.getElementById('contact-form');
-        const cform = document.getElementById('contact-form');
+    });
         
-        cform.addEventListener('submit', (e) => {
-          e.preventDefault();
-          const firstName = document.querySelector('input[placeholder="Shivam"]').value;
-          const lastName = document.querySelector('input[placeholder="Singh"]').value;
-          const email = document.querySelector('input[placeholder="shivamsingh910672@gmail.com"]').value;
-          const mobile = document.querySelector('input[placeholder="XXXXXXXXX"]').value;
-          const message = document.querySelector('textarea').value;
-          const subject = 'New message from your portfolio website';
-          const body = `First Name: ${firstName}\nLast Name: ${lastName}\nEmail: ${email}\nMobile: ${mobile}\nMessage: ${message}`;
-          const mailtoLink = `mailto:shivamsingh910672@gmail.com?subject=${subject}&body=${body}`;
-          window.location.href = mailtoLink;
-        });
+    document.getElementById('year').textContent = new Date().getFullYear();const form = document.getElementById('contact-form');
+    
+    const cform = document.getElementById('contact-form');
+        
+    cform.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const firstName = document.querySelector('input[placeholder="Shivam"]').value;
+      const lastName = document.querySelector('input[placeholder="Singh"]').value;
+      const email = document.querySelector('input[placeholder="shivamsingh910672@gmail.com"]').value;
+      const mobile = document.querySelector('input[placeholder="XXXXXXXXX"]').value;
+      const message = document.querySelector('textarea').value;
+      const subject = 'New message from your portfolio website';
+      const body = `First Name: ${firstName}\nLast Name: ${lastName}\nEmail: ${email}\nMobile: ${mobile}\nMessage: ${message}`;
+      const mailtoLink = `mailto:shivamsingh910672@gmail.com?subject=${subject}&body=${body}`;
+      window.location.href = mailtoLink;
     });
 });
 
